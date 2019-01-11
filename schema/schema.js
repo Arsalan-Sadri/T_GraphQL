7f578c9fa9c591f28a9403cd4308c9bdccb797ac
Arsalan Sadri <asadri62@gmail.com>
2019-01-11 15:39:42 -0800

    Adding a RootQuery const

--- schema/schema.js
+++ schema/schema.js
@@ -16,3 +16,21 @@ const BookType = new GraphQLObjectType({
         }
     })
 });
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        book: {
+            // name of the query
+            type: BookType, // the node on which the query will be made
+            args: {
+                // arguments to be passed along with the query
+                id: {
+                    // the id of the book
+                    type: GraphQLString
+                }
+            },
+            resolve(parent, args) {}
+        }
+    }
+});
