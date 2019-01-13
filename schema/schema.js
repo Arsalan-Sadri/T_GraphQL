5f4692fdd4faff196a0d237deff597fd76067e77
Arsalan Sadri <asadri62@gmail.com>
2019-01-13 12:01:22 -0800

    Adding a books variable as a data to schema.js

--- schema/schema.js
+++ schema/schema.js
@@ -37,6 +37,24 @@ const RootQuery = new GraphQLObjectType({
     }
 });
 
+const books = [
+    {
+        name: "Name of the Wind",
+        genre: "Fantasy",
+        id: "1"
+    },
+    {
+        name: "The Final Empire",
+        genre: "Fantasy",
+        id: "2"
+    },
+    {
+        name: "The Long Earth",
+        genre: "Sci-Fi",
+        id: "3"
+    }
+];
+
 module.exports = new GraphQLSchema({
     // which query is available to user to use
     query: RootQuery
